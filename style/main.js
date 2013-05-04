36c9ce1ba71d348797f25d23312ff42bf5123015
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-05-04 23:46:20 +0300

    Более точная реакция на снятие выделения текста

--- style/main.js
+++ style/main.js
@@ -1,9 +1,18 @@
 
 (function () {
-    var debugHighlight = function () {
+    var debugHighlightChecker = function () {
         var className = window.getSelection().isCollapsed ? '' : 'debug';
         document.getElementsByTagName('body')[0].className = className;
     };
-    document.addEventListener('mouseup', debugHighlight);
-    document.addEventListener('keyup', debugHighlight);
+
+    var debugHighlightHandler = function() {
+        debugHighlightChecker();
+
+        // если одиночный щелчок мышью пришёлся по выделению, то
+        // событие может прийти раньше, чем выделение снимается
+        window.setTimeout(debugHighlightChecker, 100);
+    };
+
+    document.addEventListener('mouseup', debugHighlightHandler);
+    document.addEventListener('keyup', debugHighlightHandler);
 })();
