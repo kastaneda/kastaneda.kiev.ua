00bbe7e238024ddd955f5878bd50b744c3b053f0
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-07-28 16:30:57 +0300

    Обратно в .kiev.ua

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # crontab:
-# */1  *  *  *  *  /home/gray/build/kastaneda.me/build.sh
+# */1  *  *  *  *  /home/gray/build/kastaneda.kiev.ua/build.sh
 
 # webhook:
-# <?php echo touch('/home/gray/build/kastaneda.me/_build') ? 'OK' : 'Error';
+# <?php echo touch('/home/gray/build/kastaneda.kiev.ua/_build') ? 'OK' : 'Error';
 
 FLAG_START=_build
 FLAG_LOCK=_build_lock
