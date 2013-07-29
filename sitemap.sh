4d8398da3bdce90e3bf5465356f7485a4a1740ea
Dmitry Kolesnikov <kastaneda@gmail.com>
2013-07-29 09:57:55 +0300

    Обратно в .kiev.ua

--- sitemap.sh
+++ sitemap.sh
@@ -6,7 +6,7 @@ echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 for FILE in "$@"
 do
   LASTMOD=`date --iso-8601=seconds --utc --reference="$FILE"`
-  URL=`echo "http://kastaneda.me/$FILE" | sed s/index.html$//`
+  URL=`echo "http://kastaneda.kiev.ua/$FILE" | sed s/index.html$//`
   echo '  <url>'
   echo "    <loc>$URL</loc>"
   echo "    <lastmod>$LASTMOD</lastmod>"
