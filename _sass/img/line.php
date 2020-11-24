ac8734990a17b8a2fc49c59f8f9e62ba40888975
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-11-24 04:54:56 +0200

    Rename background grid files

--- _sass/img/line.php
+++ _sass/img/line.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-$img = file_get_contents($argv[1] ?? 'line-height-24px.png');
+$img = file_get_contents($argv[1] ?? 'grid-bg-24px.png');
 $bg = 'data:image/png;base64,' . base64_encode($img);
 
 echo "body.js-selection\n  background: \$background-color url($bg) left top\n";
