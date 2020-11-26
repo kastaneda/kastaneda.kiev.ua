2d2bf0ce732f96b36e12a8a4e1bde5d01c6821d5
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-11-27 00:35:59 +0200

    Indentation fix

--- _includes/main.js
+++ _includes/main.js
@@ -1,20 +1,20 @@
 (function () {
-    var debugHighlightChecker = function () {
-        var className = 'js-selection';
-        var body = document.getElementsByTagName('body')[0];
+  var debugHighlightChecker = function () {
+    var className = 'js-selection';
+    var body = document.getElementsByTagName('body')[0];
 
-        if (window.getSelection().isCollapsed) {
-            body.classList.remove(className);
-        } else {
-            body.classList.add(className);
-        }
-    };
+    if (window.getSelection().isCollapsed) {
+      body.classList.remove(className);
+    } else {
+      body.classList.add(className);
+    }
+  };
 
-    var debugHighlightHandler = function() {
-        debugHighlightChecker();
-        window.setTimeout(debugHighlightChecker, 100);
-    };
+  var debugHighlightHandler = function() {
+    debugHighlightChecker();
+    window.setTimeout(debugHighlightChecker, 100);
+  };
 
-    document.addEventListener('mouseup', debugHighlightHandler);
-    document.addEventListener('keyup', debugHighlightHandler);
+  document.addEventListener('mouseup', debugHighlightHandler);
+  document.addEventListener('keyup', debugHighlightHandler);
 })();
